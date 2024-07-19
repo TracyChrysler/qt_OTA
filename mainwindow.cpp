d89e127e33624362b1fa9e345177cdd78cc91492
yanhongzhi <1620025565@qq.com>
2024-07-19 17:23:03 +0800

    browseButten and documentPath

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QDebug>
+#include <QFile>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->browseButton, &QPushButton::clicked, this, &MainWindow::on_browseButton_clicked);
+
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +20,27 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_browseButton_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,tr("Open File"),"",tr("All File(*)"));
+    if(fileName.isEmpty()){
+        //如果未选择文件，弹出警告框
+        QMessageBox::warning(this,tr("警告"),tr("未选择文件！"));
+    }
+    else{
+        ui->documentPath->setText(fileName);
+    }
+    //读取文件内容并显示到ui->filecontent中
+    QFile file(fileName);
+    if(!file.open(QIODevice::ReadOnly | QIODevice::Text)){
+        QMessageBox::critical(this,tr("错误"),tr("无法打开文件！"));
+        return;
+    }
+    QTextStream in(&file);
+    QString fileContent = in.readAll();
+    ui->fileContent->setPlainText(fileContent);
+
+    file.close();
+
+}
